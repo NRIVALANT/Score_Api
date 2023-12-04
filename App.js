1f101b6965fe2d669f9e939144949ffa07178583
NRIVALANT <nicolas.rivalant@ynov.com>
2023-12-04 19:16:46 +0100

    connect doesnt work

--- App.js
+++ App.js
@@ -1,11 +1,39 @@
-//app
-const express = require("express")
-const mongoose = require("mongoose")
-const compression = require("compression")
+const express = require('express');
+const mongoose = require('mongoose');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const ENV = require('./environment/environment');
 
 const app = express();
-
 app.use(compression());
-const apiRoutes = require("./routes/route")
-app.use("/", apiRoutes)
 
+const db_Id = ENV.DB_ID;
+const DB_pw = ENV.DB_PASSWORD;
+const DB = 'mongodb+srv://' + db_Id + ':' + DB_pw + '@api-test.yblbghu.mongodb.net/';
+
+
+// Passby CORS errors
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+mongoose.connect(DB).then(() => {
+    console.log('Connected to MongoDB with Success !');
+}).catch((err) => {
+    console.log('MongoDB ERROR', err);
+});
+
+
+app.use(bodyParser.json());
+// ROUTES
+// const apiRoutes = require('./routes/api');
+// const pokemonRoutes = require('./routes/pokemon');
+// const userRoutes = require('./routes/user');
+// app.use('/api/', apiRoutes);
+// app.use('/api/pokemons/', pokemonRoutes);
+// app.use('/api/users/', userRoutes);
+
+module.exports = app;
---
+++ environment/environment.js
@@ -0,0 +1,8 @@
+const env = {
+    PORT : 3000,
+    DB_ID : 'nicolasrivalant',
+    DB_PASSWORD : 'Nicol@s2002',
+ 
+};
+
+module.exports = env;
